e484a2e7667a2fe8dfce28b48fdeb0ea20340ffd
Bruno Dechen <45558134+d3ch3n@users.noreply.github.com>
2025-10-14 13:30:10 -0300

    Main Version

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "DCH~WyreStorm MX-0808-SCL",
-  Version = "1.0",
-  BuildVersion = "1.0.0.248",
+  Version = "2.0",
+  BuildVersion = "2.0.1.0",
   Id = "1dce4a60-da90-4e31-ac50-97b876900199",
   Author = "Dechen Technology",
   Description = "Control WyreStorm MX-0808-SCL HDMI Matrix Switcher",  
